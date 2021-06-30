51865f4f2b87299a5e7fd83646ed173d256cef95
bluephoenix5 <mia.a.lord@gmail.com>
2021-06-30 15:01:06 -0700

    EOD draft

--- CarTests/CarTests.cs
+++ CarTests/CarTests.cs
@@ -1,13 +1,56 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarNS;
 
 namespace CarTests
 {
+    [TestClass]
     public class CarTests
     {
         //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitHub account)
+        [TestMethod]
+        public void EmptyTest()
+        {
+            Assert.AreEqual(10, 10, .001);
+            Assert.AreEqual(10, 10.0009, .001);
+        }
+
         //TODO: constructor sets gasTankLevel properly
+        Car test_car;
+        
+        [TestInitialize]
+        public void CreateCarObject()
+        {
+            test_car = new Car("Toyota", "Prius", 10, 50, 4);
+        }
+        
+        [TestMethod]
+        public void TestInitialGasTank()
+        {
+            Assert.AreEqual(10, test_car.GasTankLevel, .001);
+        }
+
+        [TestMethod]
+        public void TestNumberOfWheels()
+        {
+            Assert.AreEqual(4, test_car.NumberOfWheels);
+        }
+
         //TODO: gasTankLevel is accurate after driving within tank range
+        [TestMethod]
+        public void TestLevelInRange()
+        {
+            test_car.Drive(50);
+            Assert.AreEqual(9, test_car.GasTankLevel, .001);
+        }
+
         //TODO: gasTankLevel is accurate after attempting to drive past tank range
+        [TestMethod]
+        public void TestLevelOutOfRange()
+        {
+            test_car.Drive(60);
+            Assert.AreEqual(9, test_car.GasTankLevel, .001);
+        }
+
         //TODO: can't have more gas than tank size, expect an exception
 
     }
